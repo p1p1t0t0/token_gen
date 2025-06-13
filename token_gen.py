7e18c343cce84016c564fe860d2c251b7b6a27e3
P1p1t0t0 <149186990+p1p1t0t0@users.noreply.github.com>
2025-06-13 14:37:37 +0200

    Create token_gen.py

---
+++ token_gen.py
@@ -0,0 +1,18 @@
+============================== 🔥 Discord Token Generator - by p1p1t0t0
+Hi, I coded you a token generator.
+
+🛠️ Features :
+
+Fast user token Generation
+Green = Valid Red = Invalid
+▶️ How to launch it ?
+Install dependencies
+
+pip install -r requirements.txt
+
+Then run the script :
+
+python token_gen.py
+
+📦 The requirements :
+pip install colorama requests pyfiglet
